ae75bccdbd274fa427847d8ee51e8fe477c11643
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-07-08 14:34:04 -0700

    commit tree 4448741fbed7e58703a1edc584417ec18759d952

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentenceArray = self.split(Regexp.union([".", "!", "?"]))
+    count = 0
+    sentenceArray.each do |sentence|
+      if sentence != "" && sentence != "." && sentence != "!" && sentence != "?"
+        count += 1
+      end
+    end
+    count
   end
 end
+
+
+
